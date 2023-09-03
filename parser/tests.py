4608d601439b527ec34a2b2725b74e329c0de57f
TheLordVier <messi060591@gmail.com>
2023-09-03 13:37:21 +0300

    add tests.py to project

--- parser/tests.py
+++ parser/tests.py
@@ -1,30 +1,33 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from parser.models import Problem
+from .models import Problem
 
 
 class ProblemTestCase(APITestCase):
     def setUp(self) -> None:
-        """Create test db"""
+        """
+        Create test db
+        """
         self.url = '/parser/'
         self.data = {
             'themes': 'test',
             'number_solutions': 20,
             'problem_name': 'test',
-            'number': 'test',
+            'number': '12345',
         }
         self.problem = Problem.objects.create(**self.data)
 
-    def test_1_create_problem(self) -> None:
-        """Problem creation testing """
-
+    def test_create_problem(self) -> None:
+        """
+        Problem creation testing
+        """
         response = self.client.post(f'{self.url}add_problems/')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertNotEqual(Problem.objects.all().count(), 0)
 
-    def test_2_list_problem(self) -> None:
+    def test_list_problem(self) -> None:
         """Problem list testing """
         response = self.client.get(f'{self.url}problems/')
 
@@ -34,14 +37,54 @@ class ProblemTestCase(APITestCase):
             [{'problem_name': 'test', 'rating': None, }]
         )
 
-    def test_4_retrieve_problem(self) -> None:
-        """Problem retrieve testing """
-
+    def test_retrieve_problem(self) -> None:
+        """
+        Problem retrieve testing
+        """
         response = self.client.get(f'{self.url}problems/{self.problem.pk}/')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.assertEqual(
             response.json(),
-            {'problem_name': 'test', 'rating': None, 'themes': 'test', 'number_solutions': 10, 'number': 'test'}
+            {'problem_name': 'test', 'rating': None, 'themes': 'test', 'number_solutions': 20, 'number': '12345'}
         )
+
+
+# import pytest
+# from rest_framework import status
+# from parser.models import Problem
+#
+#
+# @pytest.mark.django_db
+# def test_create_problem(api_client):
+#     """Problem creation testing """
+#
+#     response = api_client.post('/parser/add_problems/')
+#
+#     assert response.status_code == status.HTTP_201_CREATED
+#     assert Problem.objects.all().count() != 0
+#
+#
+# @pytest.mark.django_db
+# def test_list_problems(api_client, sample_problem):
+#     """Problem list testing """
+#
+#     response = api_client.get('/parser/problems/')
+#
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == [
+#         {'problem_name': 'Sample Problem', 'rating': 5, 'themes': 'sample theme', 'number_solutions': 10,
+#          'number': '12345'}]
+#
+#
+# @pytest.mark.django_db
+# def test_retrieve_problem(api_client, sample_problem):
+#     """Problem retrieve testing """
+#
+#     response = api_client.get(f'/parser/problems/{sample_problem.pk}/')
+#
+#     assert response.status_code == status.HTTP_200_OK
+#
+#     assert response.json() == {'problem_name': 'Sample Problem', 'rating': 5, 'themes': 'sample theme',
+#                                'number_solutions': 10, 'number': '12345'}
